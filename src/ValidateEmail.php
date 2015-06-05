9b3e68abca47935ce5cd45bb0ed4aa5ef50a1cda
Colin DeCarlo <colin@thedecarlos.ca>
2015-06-04 21:27:10 -0400

    Reduce indent one level on the whole file

--- src/ValidateEmail.php
+++ src/ValidateEmail.php
@@ -1,142 +1,143 @@
 <?php
-	namespace ServiceTo;
 
-	use Exception;
+namespace ServiceTo;
 
-	class ValidateEmail {
-		/**
-		 * The address to supply in our MAIL FROM connection to the SMTP servers we're talking to.
-		 *
-		 * @var string
-		 */
-		private $testaddress = "validateemail@service.to";
+use Exception;
 
-		/**
-		 * Alias to lookup
-		 *
-		 * @param  string  $emailaddress  email address to test
-		 * @return boolean
-		 */
- 		function test($emailaddress) {
-			return $this->lookup($emailaddress);
-		}
+class ValidateEmail {
+    /**
+     * The address to supply in our MAIL FROM connection to the SMTP servers we're talking to.
+     *
+     * @var string
+     */
+    private $testaddress = "validateemail@service.to";
 
-		/**
-		 * Lookup all MX records and check each until we have a success
-		 *
-		 * @param  string  $emailaddress  email address to look up
-		 * @return boolean
-		 */
-		function lookup($emailaddress) {
-			list($user, $domain) = preg_split("/@/", trim($emailaddress));
+    /**
+     * Alias to lookup
+     *
+     * @param  string  $emailaddress  email address to test
+     * @return boolean
+     */
+    function test($emailaddress) {
+        return $this->lookup($emailaddress);
+    }
 
-			if ($user == "") {
-				throw new ValidateEmailException("Blank user name");
-			}
-			if ($domain == "") {
-				throw new ValidateEmailException("Blank domain name");
-			}
+    /**
+     * Lookup all MX records and check each until we have a success
+     *
+     * @param  string  $emailaddress  email address to look up
+     * @return boolean
+     */
+    function lookup($emailaddress) {
+        list($user, $domain) = preg_split("/@/", trim($emailaddress));
 
-			$mxhosts = array();
-			$weight = array();
-			if (getmxrr($domain, $mxhosts, $weight)) {
-				// pick first one and check it.
+        if ($user == "") {
+            throw new ValidateEmailException("Blank user name");
+        }
+        if ($domain == "") {
+            throw new ValidateEmailException("Blank domain name");
+        }
 
-				array_multisort($weight, $mxhosts);
+        $mxhosts = array();
+        $weight = array();
+        if (getmxrr($domain, $mxhosts, $weight)) {
+            // pick first one and check it.
 
-				foreach ($mxhosts as $id => $mxhost) {
-					if ($this->verify($emailaddress, $mxhost)) {
-						return true;
-					}
-				}
-			}
-			else {
-				throw new ValidateEmailException("No MX records");
-			}
-		}
+            array_multisort($weight, $mxhosts);
 
-		/**
-		 * Connect to the mail server on port 25 and see if it allows mail for the users' supplied email address.
-		 *
-		 * @param  string  $emailaddress  email address to test
-		 * @param  string  $mxhost        mail server host name to connect to and test
-		 * @return boolean
-		 */
-		function verify($emailaddress, $mxhost) {
-			$validated = false;
+            foreach ($mxhosts as $id => $mxhost) {
+                if ($this->verify($emailaddress, $mxhost)) {
+                    return true;
+                }
+            }
+        }
+        else {
+            throw new ValidateEmailException("No MX records");
+        }
+    }
 
-			$socket = stream_socket_client("tcp://" . $mxhost . ":25", $errno, $errstr, 30);
-			stream_set_blocking($socket, true);
+    /**
+     * Connect to the mail server on port 25 and see if it allows mail for the users' supplied email address.
+     *
+     * @param  string  $emailaddress  email address to test
+     * @param  string  $mxhost        mail server host name to connect to and test
+     * @return boolean
+     */
+    function verify($emailaddress, $mxhost) {
+        $validated = false;
 
-			$buffer = null;
-			// server will say hi...
-			$buffer .= fgets($socket, 2048);
+        $socket = stream_socket_client("tcp://" . $mxhost . ":25", $errno, $errstr, 30);
+        stream_set_blocking($socket, true);
 
-			$response = trim($buffer, "\r\n.");
-			$buffer = null;
+        $buffer = null;
+        // server will say hi...
+        $buffer .= fgets($socket, 2048);
 
-			list($code, $message) = preg_split("/\s+/", $response, 2);
-			if ($code == 220) {
-				// say hello.
-				$message = "EHLO ValidateEmail\r\n";
-				fwrite($socket, $message);
+        $response = trim($buffer, "\r\n.");
+        $buffer = null;
 
-				while($buf = fgets($socket)) {
-					$buffer .= $buf;
-					list($code, $message) = preg_split("/\s+/", $buf, 2);
-					if ($code == "250") {
-						break;
-					}
-				}
-				$response = trim($buffer, "\r\n");
-				$buffer = null;
+        list($code, $message) = preg_split("/\s+/", $response, 2);
+        if ($code == 220) {
+            // say hello.
+            $message = "EHLO ValidateEmail\r\n";
+            fwrite($socket, $message);
 
-				$lines = preg_split("/\n/", $response);
-				$last = count($lines) - 1;
-				list($code, $message) = preg_split("/\s+/", $lines[$last], 2);
-				if ($code == 250) {
-					// give them my from address
-					$message = "MAIL FROM:<" . $this->testaddress . ">\r\n";
-					fwrite($socket, $message);
+            while($buf = fgets($socket)) {
+                $buffer .= $buf;
+                list($code, $message) = preg_split("/\s+/", $buf, 2);
+                if ($code == "250") {
+                    break;
+                }
+            }
+            $response = trim($buffer, "\r\n");
+            $buffer = null;
 
-					$buffer .= fgets($socket, 2048);
+            $lines = preg_split("/\n/", $response);
+            $last = count($lines) - 1;
+            list($code, $message) = preg_split("/\s+/", $lines[$last], 2);
+            if ($code == 250) {
+                // give them my from address
+                $message = "MAIL FROM:<" . $this->testaddress . ">\r\n";
+                fwrite($socket, $message);
 
-					$response = trim($buffer, "\r\n");
-					$buffer = null;
+                $buffer .= fgets($socket, 2048);
 
-					list($code, $message) = preg_split("/\s+/", $response, 2);
+                $response = trim($buffer, "\r\n");
+                $buffer = null;
 
-					if ($code == 250) {
-						// give them the user's address.
-						$message = "RCPT TO:<" . $emailaddress . ">\r\n";
-						fwrite($socket, $message);
+                list($code, $message) = preg_split("/\s+/", $response, 2);
 
-						$buffer .= fgets($socket, 2048);
+                if ($code == 250) {
+                    // give them the user's address.
+                    $message = "RCPT TO:<" . $emailaddress . ">\r\n";
+                    fwrite($socket, $message);
 
-						$response = trim($buffer, "\r\n");
-						$buffer = null;
+                    $buffer .= fgets($socket, 2048);
 
-						list($code, $message) = preg_split("/\s+/", $response, 2);
-						if ($code == 250) {
-							$validated = true;
-						}
+                    $response = trim($buffer, "\r\n");
+                    $buffer = null;
 
-						// say goodbye regardless
-						$message = "QUIT\r\n";
-						fwrite($socket, $message);
+                    list($code, $message) = preg_split("/\s+/", $response, 2);
+                    if ($code == 250) {
+                        $validated = true;
+                    }
 
-						$buffer .= fgets($socket, 2048);
+                    // say goodbye regardless
+                    $message = "QUIT\r\n";
+                    fwrite($socket, $message);
 
-						$response = trim($buffer, "\r\n");
-						$buffer = null;
+                    $buffer .= fgets($socket, 2048);
 
-						list($code, $message) = preg_split("/\s+/", $response, 2);
+                    $response = trim($buffer, "\r\n");
+                    $buffer = null;
 
-						return $validated;
-					}
-				}
-			}
-		}
-	}
+                    list($code, $message) = preg_split("/\s+/", $response, 2);
 
-	class ValidateEmailException extends Exception {}
+                    return $validated;
+                }
+            }
+        }
+    }
+}
+
+class ValidateEmailException extends Exception {}
